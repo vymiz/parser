b78d2d30d526c60a3ba777578126d670cd953305
vymiz <vymiz020777@gmail.com>
2021-10-13 10:43:29 +0300

    parser_yandex_2 is ready

---
+++ parser_yandex_2.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://yandex.ru'
+page = requests.get(url)
+filtered_news = []
+all_news = []
+# soup = BeautifulSoup(page.text, 'html.parser')
+soup = BeautifulSoup(page.text, 'lxml')
+
+txt = []
+ol = soup.ol
+a = ol.find_all('a')
+for _ in a:
+    txt.append((_.get('aria-label')))
+
+title = []
+div = ol.find_all('div')
+for _ in div:
+    title.append((_.get('title')))
+
+res = []
+for i in range(len(txt)):
+    s = title[i] + ': ' + txt[i]
+    res.append(s)
+
+for i in res:
+    print(i)
+
